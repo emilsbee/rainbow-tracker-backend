906e40e50a526bc7cabcd3dc19e3247100296084
emils <emilsbernhards2001@gmail.com>
2021-07-03 13:41:59 +0200

    node-pg config is now dynamically taken from .env based on the NODE_ENV environment.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,28 @@
 // External imports
-import {PoolClient, Query, QueryResult} from "pg";
+import {PoolClient, Query} from "pg";
 
 const { Pool } = require('pg')
 
-const pool = new Pool()
+// get the current environment
+let env = process.env.NODE_ENV
+
+// convert to uppercase
+let envString = env.toUpperCase()
+
+// access the environment variables for this environment
+let pgpassword = process.env['PGPASSWORD_' + envString]
+let pguser = process.env['PGUSER_' + envString]
+let pghost = process.env['PGHOST_' + envString]
+let pgdatabase = process.env['PGDATABASE_' + envString]
+let pgport = process.env['PGPORT_' + envString]
+
+const pool = new Pool({
+    password: pgpassword,
+    user: pguser,
+    host: pghost,
+    database: pgdatabase,
+    port: pgport
+})
 
 module.exports = {
     query: async (text:string, params:any[]):Promise<Query> => {
