bf11c7ae00af9d589a2553790c282010906dcfda
Emils Bernhards <bernhardsemils@gmail.com>
2021-07-04 11:04:15 +0200

    Added secure attribute to session cookie when env.NODE_ENV is not development.

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ const CONFIG = {
     overwrite: true, /** (boolean) can overwrite or not (default true) */
     httpOnly: true, /** (boolean) httpOnly or not (default true) */
     signed: true, /** (boolean) signed or not (default true) */
+    secure: process.env.NODE_ENV !== "development",
     rolling: true, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. default is false **/
 };
 
