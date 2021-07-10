c9c8f1a5507ac12fbf7a8ee8cd4df922cd6387f5
Emils Bernhards <bernhardsemils@gmail.com>
2021-07-10 19:05:13 +0200

    Redis host is determined from an environment variable.

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -2,10 +2,11 @@
 const util = require('util');
 import redis, {RedisClient} from "redis";
 
-const redisClient:RedisClient = redis.createClient();
+const redisClient:RedisClient = redis.createClient({
+    host: process.env.REDIS_HOST
+});
 redisClient.set = util.promisify(redisClient.set).bind(redisClient);
 redisClient.get = util.promisify(redisClient.get).bind(redisClient);
 redisClient.del = util.promisify(redisClient.del).bind(redisClient)
 redisClient.setex = util.promisify(redisClient.setex).bind(redisClient)
-
 export default redisClient
