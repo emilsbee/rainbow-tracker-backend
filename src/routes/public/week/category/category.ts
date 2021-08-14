8eb7a849f7f3c148466904c81b6bc0d3cc1e3264
Emils Bernhards <bernhardsemils@gmail.com>
2021-08-14 14:40:18 +0200

    Added documentation.

--- src/routes/public/week/category/category.ts
+++ src/routes/public/week/category/category.ts
@@ -10,6 +10,12 @@ import {Category} from "../week";
 
 let router = new Router(); // Initialize router
 
+/**
+ * Route for updating a given week day's categories. So all categories from a specific week
+ * and a specific day are updated. To be more specific, the aspects of categories that are
+ * updated are the categoryid and activityid. However, all the information of a category has to
+ * be provided like the categoryPosition, weekid, etc.
+ */
 router.patch("/categories", contentType.JSON, protect.user, async (ctx: Context) => {
     let weekid = ctx.params.weekid
     let userid = ctx.params.userid
