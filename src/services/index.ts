017d8fb64a6f1018eac04fc0a23658648b497e3a
Emils Bernhards <bernhards@emils.xyz>
2022-02-20 16:52:40 +0100

    Fix import that doesnt exist

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,5 @@ export * from "./checkEnvVariables";
 export * from "./generateStaticData";
 export * from "./groupByDays";
 export * from "./prismaClient";
-export * from "./serveFrontend";
 export * from "./setupMiddleware";
 export * from "./setupRoutes";
